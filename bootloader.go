2ccc5ad051887e7048c844b8b49925392b854b18
Graham Newton <graham@tripiq.eu>
2017-08-27 20:40:38 +0100

    Added a method to auto create a config and create a global installation symlink

--- bootloader.go
+++ bootloader.go
@@ -9,19 +9,21 @@ func Bootstrap() {
 	ParseFlags()
 	ParseArgs()
 
-	if FLAG_SHOW_HELP {
+	if *FLAG_SHOW_HELP {
 		ShowHelp()
 		os.Exit(1)
 	}
 
-	var configFileExists bool = ConfigExists()
-	if !configFileExists {
-		ConsoleWrite(ConsoleInterface{
-			Message: "Config does not exist, please create a " + CONFIG_DEFAULT_FILE_NAME,
-			Severity: "Error",
-			Terminate: true,
-		})
-	}
+	if GetFriendlyArgName() != "create-config" {
+		var configFileExists bool = ConfigExists()
+		if !configFileExists {
+			ConsoleWrite(ConsoleInterface{
+				Message: "Config does not exist, please create a " + CONFIG_DEFAULT_FILE_NAME,
+				Severity: "Error",
+				Terminate: true,
+			})
+		}
 
-	ParseConfig()
+		ParseConfig()
+	}
 }
--- dispatch.go
+++ dispatch.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path"
-	"strings"
+	"path/filepath"
 	"time"
 )
 
 func Dispatch(s string) {
-	switch strings.ToLower(s) {
+	switch GetFriendlyArgName() {
 	case "update":
 		GitUpdate()
 		MapFiles(APPLICATION_CONFIG_SETTINGS.Links, false)
 		MapFiles(APPLICATION_CONFIG_SETTINGS.Sources, true)
+		break
+
+	case "create-config":
+		CreateConfig()
+		break
+
+	case "purge-backups":
+		if err := PurgeBackups(); err != nil {
+			ConsoleWrite(ConsoleInterface{
+				Error:     err,
+				Terminate: true,
+			})
+		}
+		break
+
+	case "create-global-installation":
+		CreateGlobalInstallation()
 		break
 
 	default:
@@ -31,13 +49,13 @@ func MapFiles(files []string, sources bool) {
 		CreateBackupDirectory()
 	}
 
-	for _, file := range files {
-		var originalFile string = path.Join(CONFIG_DEFAULT_FILE_LOCATION, file)
-		var vcsFile string = path.Join(CONFIG_DOT_FILES_LOCATION, file)
+	for _, f := range files {
+		var originalFile string = path.Join(CONFIG_DEFAULT_FILE_LOCATION, f)
+		var vcsFile string = path.Join(CONFIG_DOT_FILES_LOCATION, f)
 
 		if pathExists(originalFile) {
 			dateTime := time.Now()
-			backupFileName := file + "_" + dateTime.Format("20060102150405")
+			backupFileName := f + "_" + dateTime.Format("20060102150405")
 			CopyFile(originalFile, path.Join(APPLICATION_CONFIG_SETTINGS.BackupPath, backupFileName))
 			os.Remove(originalFile)
 
@@ -55,7 +73,7 @@ func MapFiles(files []string, sources bool) {
 
 		if sources {
 			ConsoleWrite(ConsoleInterface{
-				Message: "Run source " + file,
+				Message:  "Run source " + f,
 				Severity: "Info",
 			})
 		}
@@ -69,4 +87,119 @@ func CreateBackupDirectory() {
 	}
 
 	os.Mkdir(dir, 0755)
+}
+
+func CreateConfig() {
+	if exists := ConfigExists(); exists {
+		ConsoleWrite(ConsoleInterface{
+			Message:   "There is already config located: " + CONFIG_DEFAULT_FILE_NAME,
+			Severity:  "Warn",
+			Terminate: true,
+		})
+	}
+
+	var conf ConfigSettings = ConfigSettings{
+		Git: GitSettings{
+			Remote:     "git url",
+			RemoteName: "origin",
+			Branch:     "master",
+			Auth: GitAuthSettings{
+				PrivateKey: path.Join(CONFIG_DEFAULT_FILE_LOCATION, "/.ssh/"),
+				UserName:   "git username",
+				Password:   "git password",
+			},
+		},
+		Backup:     true,
+		BackupPath: path.Join(CONFIG_DEFAULT_FILE_LOCATION, "/.dotfiles_backup/"),
+		Links: []string{
+			".bashrc",
+		},
+		Sources: []string{
+			".macos",
+		},
+	}
+
+	jsonPayload, err := json.MarshalIndent(conf, "", "    ")
+	if err != nil {
+		ConsoleWrite(ConsoleInterface{
+			Error:     err,
+			Terminate: true,
+		})
+	}
+
+	f, err := os.Create(CONFIG_DEFAULT_FILE_NAME)
+	if err != nil {
+		ConsoleWrite(ConsoleInterface{
+			Error:     err,
+			Terminate: true,
+		})
+	}
+
+	defer f.Close()
+
+	if _, err := f.Write(jsonPayload); err != nil {
+		ConsoleWrite(ConsoleInterface{
+			Error:     err,
+			Terminate: true,
+		})
+	}
+
+	if err := f.Sync(); err != nil {
+		ConsoleWrite(ConsoleInterface{
+			Error:     err,
+			Terminate: true,
+		})
+	}
+
+	ConsoleWrite(ConsoleInterface{
+		Message:   "Successfully created example config file located: " + CONFIG_DEFAULT_FILE_NAME,
+		Severity:  "Info",
+		Terminate: true,
+	})
+}
+
+func PurgeBackups() error {
+	d, err := os.Open(APPLICATION_CONFIG_SETTINGS.BackupPath)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(APPLICATION_CONFIG_SETTINGS.BackupPath, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func CreateGlobalInstallation() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		ConsoleWrite(ConsoleInterface{
+			Error:     err,
+			Terminate: true,
+		})
+	}
+
+	var globalPath string = path.Join("/usr/local/bin/", os.Args[0])
+	var selfPath string = path.Join(dir, os.Args[0])
+
+	if err := os.Symlink(selfPath, globalPath); err != nil {
+		ConsoleWrite(ConsoleInterface{
+			Message:   "Failed to create global installation",
+			Error:     err,
+			Terminate: true,
+		})
+	}
+
+	ConsoleWrite(ConsoleInterface{
+		Message:   "Successfully create global installation in " + globalPath,
+		Severity:  "Success",
+		Terminate: true,
+	})
 }
